3b0cbf33bf9eab75de6e118ee2148b5d59aef1ab
Simvolokov Pavel <psimvolokov@gmail.com>
2024-10-27 16:35:40 +0300

    add server

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { products } from "./products";
 import { useTg } from "./hooks/useTg";
+import { serverUrl } from "./config";
 
 function App() {
   const [count, setCount] = useState(0);
-  const { user } = useTg();
+  const { user, queryId, tg } = useTg();
+
+  tg.MainButton.show();
+
+  const onSendData = useCallback(() => {
+    const data = {
+      queryId: queryId,
+      userId: user?.id,
+      products: products,
+    };
+    console.log("Sending data:", data);
+    fetch(`${serverUrl}/order`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => console.log("Success:", data))
+      .catch((error) => console.error("Error during fetch:", error));
+  }, [products, queryId]);
+
+  useEffect(() => {
+    tg.onEvent("mainButtonClicked", onSendData);
+    return () => {
+      tg.offEvent("mainButtonClicked", onSendData);
+    };
+  }, [onSendData]);
 
   return (
     <>
@@ -18,7 +52,6 @@ function App() {
             <li key={pr.id}>{pr.title}</li>
           ))}
         </ul>
-        <button>Send</button>
       </div>
     </>
   );
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const serverUrl = "https://9b49-185-33-161-183.ngrok-free.app";
